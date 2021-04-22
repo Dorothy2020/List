e957545c463192a9cb2e20480b6e132c6d90304c
Dorothy2020 <wangarraakoth@gmail.com>
2021-04-22 08:32:29 +0300

    list

---
+++ list.py
@@ -0,0 +1,19 @@
+# list concatenation
+numbers=[1,2,3,4,6]
+names=["Dorothy","Jane","Liz","Ted","Beldy"]
+
+print(numbers+names)
+
+# accessing elements by indexing
+colors=["Red","Blue","Green","Purple"]
+print(colors[0])
+print(colors[1])
+print(colors[2])
+
+
+
+
+
+
+
+
